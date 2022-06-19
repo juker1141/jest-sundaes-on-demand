1772d7a3e413257d54f9fda2a65a632c7748cc65
Ryu <ryu@realtime.tw>
2022-06-19 21:23:38 +0800

    Test and code userEvent hover with element

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,10 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("Initial conditions", () => {
   render(<SummaryForm />);
@@ -23,12 +28,38 @@ test("inital checkBox disabled with submitButton", () => {
   const confirmButton = screen.getByRole("button", { name: /Confirm order/i });
 
   // click checkBox
-  fireEvent.click(checkBox);
+  userEvent.click(checkBox);
   // expect confirmButton will be enabled
   expect(confirmButton).toBeEnabled();
 
   // click checkBox again
-  fireEvent.click(checkBox);
+  userEvent.click(checkBox);
   // expect confirmButton will be disabled
   expect(confirmButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  // because some element disappear is async.
+  // so we need to await that.
+  // if we don't await that the test will faster then element disappeared
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
